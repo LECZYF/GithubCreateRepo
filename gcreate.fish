039fc0e550531c975371d150907b8368bf7b20c0
LECZYF <filipleczycki@gmail.com>
2017-09-07 01:14:10 +0200

    Aaand boolean of course

--- gcreate.fish
+++ gcreate.fish
@@ -2,7 +2,7 @@ function gcreate
 	mkdir $argv[2]
     cd $argv[2]
     git init
-    curl -u $argv[1] https://api.github.com/user/repos -d "{\"name\": \"$argv[2]\", \"private\": \"$argv[3]\"}"
+    curl -u $argv[1] https://api.github.com/user/repos -d "{\"name\": \"$argv[2]\", \"private\": $argv[3]}"
     git remote add origin git@github.com:$argv[1]/$argv[2].git
     echo "## " $argv[2] "by" $argv[1] >> readme.md
     git add readme.md
